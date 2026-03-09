eb40dff3ef27beeed4a9974b6b7f8a90d8f0101f
Lucian Codes <ayomideayomipo147@gmail.com>
2026-03-09 19:10:57 +0100

    Updated to Modular monolith

---
+++ Backend Server/src/shared/middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+import prisma from '../config/db.js'
+
+export const authenticateToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized' })
+  }
+
+  const token = authHeader.split(' ')[1]
+  try {
+    const decoded = verifyToken(token)
+    req.user = decoded
+    next()
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+}
