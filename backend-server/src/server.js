ed97b0cda07ac6fa7e8cdf9d5144a87f88eae8e5
Lucian Codes <ayomideayomipo147@gmail.com>
2026-04-17 13:30:55 +0100

    fixed afain

--- backend-server/src/server.js
+++ backend-server/src/server.js
@@ -13,12 +13,20 @@ import userProfileRoutes from './module/User profile/userProfile.routes.js'
 dotenv.config()
 
 const app = express()
+app.set('trust proxy', 1)
 
 // Global rate limiter
 const globalLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
 	limit: 100,
 	message: 'Too many requests from this IP. Please try again later.',
+	standardHeaders: true,
+	legacyHeaders: false,
+
+	// 🔥 Fix for proxy environments
+	keyGenerator: (req) => {
+		return req.ip
+	},
 })
 
 // middleware
